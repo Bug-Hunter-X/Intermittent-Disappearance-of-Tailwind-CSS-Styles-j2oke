a57d627c682433a634bc5525bf259442e57eb8b2
BugHunter-X <askzenai@gmail.com>
2025-02-01 12:04:10 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,7 @@
+```javascript
+// ... some Tailwind CSS classes ...
+<div class="bg-gray-200 p-4 rounded-lg shadow-md">
+  <h2 class="text-xl font-bold mb-2">Uncommon Bug</h2>
+  <p>This is some text with a <span class="text-red-500">red</span> highlight that unexpectedly disappears.</p>
+</div>
+```
